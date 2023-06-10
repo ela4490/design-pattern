ad3bf33529ae7e8c8fede3aac201a0ac1d797908
Ela Singh <30351771+ela4490@users.noreply.github.com>
2023-06-10 18:46:26 +0200

    Creational Singleton Design Pattern

---
+++ creational-singleton/README.md
@@ -0,0 +1,2 @@
+# Creational Singleton Design Pattern
+Reference: https://www.youtube.com/watch?v=tSZn4wkBIu8&list=PLlsmxlJgn1HJpa28yHzkBmUY-Ty71ZUGc&index=2
---
+++ creational-singleton/src/main/java/com/design/pattern/Main.java
@@ -0,0 +1,10 @@
+package com.design.pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Singleton.getInstance("Geekific"));
+        Singleton singleton = Singleton.getInstance("Singleton");
+        System.out.println(singleton);
+        System.out.println(singleton.getData());
+    }
+}
